a87f02d94e66b4c7fc2c5da4d6fb3528afa12a3a
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-03-14 17:58:36 -0500

    Clase empleado

--- main.java
+++ main.java
@@ -1,9 +1,10 @@
 public class main {
         public static void main(String[] args) {
-            public class Empleado{
 
-            }
         }
+
+        public static class Empleado {
         }
+}
 
 
