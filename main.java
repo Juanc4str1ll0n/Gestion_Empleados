528cfd49496695a99dc499513baa0485958afcf1
Juanc4str1ll0n <juancastrillon868@gmail.com>
2025-03-14 19:25:43 -0500

    Implementacion del menu en la clase main

--- main.java
+++ main.java
@@ -1,6 +1,9 @@
 public class main {
         public static void main(String[] args) {
 
+                //Implementacion del menu en el main.
+
+                Gestion_Empleados.Menu();
 
         }
 
